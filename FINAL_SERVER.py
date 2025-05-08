a242c14bdb0907094944cd6efa221aaf3b0f71a5
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2025-05-08 23:46:44 +0530

    Remove unnecessary blank line in the prediction route definition

--- FINAL_SERVER.py
+++ FINAL_SERVER.py
@@ -79,7 +79,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
-
+# 
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'image' not in request.files:
